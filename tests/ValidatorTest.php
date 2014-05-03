fd2bc720b561555f4b11242dc75895ff5b6df7b9
dws10 <dws10@uni.brighton.ac.uk>
2014-05-03 22:12:51 +0100

    Update ValidatorTest.php

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('var/lib/hudson/jobs/marketer/marketer_new/workspace/src/class/Validator.php');
+include '/var/lib/hudson/jobs/marketer/marketer_new/workspace/src/class/Validator.php';
 
 class ValidatorTest extends \PHPUnit_Framework_TestCase
 {
