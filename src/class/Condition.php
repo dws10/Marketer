62e2705043051202726eae6dd8fecdbc0c9d0891
dws10 <dws10@uni.brighton.ac.uk>
2014-05-04 18:58:45 +0100

    Update Condition.php
    refactored and recommented

--- src/class/Condition.php
+++ src/class/Condition.php
@@ -5,11 +5,36 @@ class Condition{
 	public $ebayConditionName;
 	public $conditionDescription;
 	
-	//setup condition	
+	/*
+	* @constructor
+	* @param int $id the condition ebay id
+	* @param int $name the condition ebay name
+	* @param int $desc the condition description
+	*/	
 	public function __construct($id, $name, $desc){
-		//constructor populates class variables
+		setEbayConditionID($id);
+		setEbayConditionName($name);
+		setConditionDescription($desc);
+	}
+	/*
+	* @method setEbayConditionID sets the ebay condition id
+	* @param int $id the condition id from ebay
+	*/
+	private function setEbayConditionID($id){
 		$this->ebayConditionID = $id;
+	}
+	/*
+	* @method setEbayConditionName sets the ebay condition name
+	* @param String $name the condition name from ebay
+	*/
+	private function setEbayConditionName($name){
 		$this->ebayConditionName = $name;
+	}
+	/*
+	* @method setConditionDescription sets the additional description information
+	* @param String $desc the extra condition details
+	*/
+	private function setConditionDescription($desc){
 		$this->conditionDescription = $desc;
 	}
 }
